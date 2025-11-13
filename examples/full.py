470223fe9f0d514d58ef1b4e704c8fb7a60a8e17
Ерилов Никита <nerilov@octopeople.net>
2025-11-13 19:18:14 +0300

    Fix full example file

--- examples/full.py
+++ examples/full.py
@@ -3,7 +3,7 @@ from yaspeedtest.client import YaSpeedTest
 
 async def main():
     ya = await YaSpeedTest().create()
-    result = await ya.run()
+    result = await ya.run(10)
     print(f"Ping: {result.ping_ms:.2f} ms")
     print(f"Download: {result.download_mbps:.2f} Mbps")
     print(f"Upload: {result.upload_mbps:.2f} Mbps")
